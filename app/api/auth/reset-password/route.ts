8ee87502bdea658c78fe6a28e389c4c23df5fe17
Seav <mrrseyla.758@gmail.com>
2024-11-25 17:33:10 +0700

    feat(reset-password) :add feature reset password

--- app/api/auth/reset-password/route.ts
+++ app/api/auth/reset-password/route.ts
@@ -4,6 +4,10 @@ import { NextRequest, NextResponse } from "next/server";
 import AppError from "@/lib/server/utils/appError";
 import { ResetPasswordFormSchema } from "@/lib/definitions";
 import { hashedToken } from "@/lib/server/utils/authUtils";
+import { db } from "@/lib/initDb";
+import { User } from "@/app/api/auth/sign-in/route";
+import bcrypt from "bcryptjs";
+
 
 export const PATCH = catchAsync(async (req: NextRequest) => {
     const data = await parseJsonBody(req);
@@ -26,12 +30,45 @@ export const PATCH = catchAsync(async (req: NextRequest) => {
         throw new AppError("Current password and new password cannot be the same", 400);
     }
 
-    const hashedResetToken = hashedToken(resetToken)
+    // check if reset password token is valid
+    const hashedResetToken = hashedToken(resetToken);
+    const user = db.prepare(`
+        SELECT * FROM users WHERE passwordResetToken = ?
+    `).get(hashedResetToken) as User;
+
+    if (!user) {
+        throw new AppError("Invalid reset password token", 401);
+    }
+
+    // hash the new password
+    const newHashedPassword = await bcrypt.hash(newPassword, 12);
+    const currentDate = new Date().toISOString().slice(0, 10); // Get current date in "YYYY-MM-DD"
+    const oldPassword = user.password;
+
+    // update the user's password in the database
+    const updatePassword = db.prepare(`
+        UPDATE users 
+        SET password =?, 
+            passwordResetToken = NULL, 
+            passwordResetExpiresAt = NULL, 
+            passwordResetRequest = ?,
+            oldPassword = ?,
+            passwordChangedAt=?
+        WHERE id =?
+    `);
+    const result = updatePassword.run(newHashedPassword, 0, currentDate, oldPassword, user.id);
+
+    if (result.changes === 0) {
+        throw new AppError("Failed to update password for the user", 400);
+    }
     // perform the reset password logic
     return NextResponse.json({
         status: "success",
         message: "Reset password successfully",
         data,
-        hashedResetToken
+        user,
+        currentDate,
+        oldPassword,
+        newHashedPassword
     })
 });
