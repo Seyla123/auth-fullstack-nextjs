8351de2fd157f8791335bc41cb3b20d487bac09f
Seav <mrrseyla.758@gmail.com>
2024-11-27 12:44:40 +0700

    chore(signin) :add style image to sign in client

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -13,7 +13,8 @@ import Loading from '@/components/Loading'
 import Link from 'next/link'
 import { PasswordInput } from '@/components/ui/password-input'
 import { useState } from 'react'
-
+import Image from 'next/image'
+import AngkorImg from '@/public/angkor-img.jpg'
 export type ErrorDataType = { data: { message: string } }
 
 function Signin() {
@@ -56,57 +57,67 @@ function Signin() {
 
   return (
 
-    <main className='flex  min-h-screen items-center justify-center'>
-      <section className='shadow-lg py-6 px-4 rounded max-w-sm w-full bg-white'>
-        <h1 className='text-3xl text-center font-bold mb-4'>
-          Sign In
-        </h1>
-        <form onSubmit={handleSubmit(onSubmit)} className='grid w-full items-center gap-4 '>
-          <div className='grid w-full   items-center gap-1.5'>
-            <Label htmlFor='email'>
-              Email
-            </Label>
-            <div>
-              <Input
-                placeholder='example@mail.com'
-                type='text'
-                {...register('email')}
-                className={cn(' focus-visible:ring-0 focus-visible:ring-offset-0 ', { 'border-red-500': errors?.email })}
-              />
-              {/* {errors?.email && <p className="text-red-500 text-[12px]">{errors.email}</p>} */}
-              {errors.email && <span className="text-red-500 text-[12px]">{String(errors?.email?.message)}</span>}
-            </div>
+    <main className='flex h-screen  items-center justify-center '>
+      <section className='shadow-lg flex h-full py-3 px-3 rounded  w-full   justify-center '>
+        <div className='max-w-[40%] w-full flex justify-center  '>
+          <div className='max-w-[400px] min-w-[300px] w-full flex flex-col justify-center px-6'>
+            <h1 className='text-3xl text-center font-bold mb-4'>
+              Sign In
+            </h1>
+            <form onSubmit={handleSubmit(onSubmit)} className='grid w-full items-center gap-4 '>
+              <div className='grid w-full   items-center gap-1.5'>
+                <Label htmlFor='email' >
+                  Email <sup className='text-red-500 font-bold text-sm'>*</sup>
+                </Label>
+                <div>
+                  <Input
+                    placeholder='example@mail.com'
+                    type='text'
+                    {...register('email')}
+                    className={cn(' focus-visible:ring-0 focus-visible:ring-offset-0 ', { 'border-red-500': errors?.email })}
+                  />
+                  {/* {errors?.email && <p className="text-red-500 text-[12px]">{errors.email}</p>} */}
+                  {errors.email && <span className="text-red-500 text-[12px]">{String(errors?.email?.message)}</span>}
+                </div>
 
+              </div>
+              <div className='grid w-full  items-center gap-1.5'>
+                <Label htmlFor='password' >
+                  Password <sup className='text-red-500 font-bold text-sm'>*</sup>
+                </Label>
+                <PasswordInput
+                  placeholder='password'
+                  {...register('password')}
+                  className={cn(' focus-visible:ring-0 focus-visible:ring-offset-0 ', { 'border-red-500': errors?.password })}
+                />
+                {errors.password && <span className="text-red-500 text-[12px]">{String(errors?.password?.message)}</span>}
+              </div>
+
+              <Button disabled={isLoading} type='submit' className='bg-dark-4'>
+
+                {isLoading ? (
+                  <>
+                    <Loading title='Sign in...' />
+                  </>
+                ) : 'Sign in'}
+              </Button>
+            </form>
+            <Link href='/sign-up'>
+              <p className='text-center text-sm mt-4'>
+                Don&apos;t have an account? Sign up
+              </p>
+            </Link>
           </div>
-          <div className='grid w-full  items-center gap-1.5'>
-            <Label htmlFor='password'>
-              Password
-            </Label>
-            <PasswordInput
-              placeholder='password'
-              value={currentPassword}
-              {...register('password')}
-              onChange={(e) => setCurrentPassword(e.target.value)}
-              autoComplete="current-password"
-              className={cn(' focus-visible:ring-0 focus-visible:ring-offset-0 ', { 'border-red-500': errors?.password })}
-            />
-            {errors.password && <span className="text-red-500 text-[12px]">{String(errors?.password?.message)}</span>}
-          </div>
+        </div>
+        <div className="h-full w-full bg-dark-2 rounded-3xl hidden lg:block relative">
+          <Image
+            src={AngkorImg}
+            alt="auth"
+            fill // Makes the image fill the parent div
+            className="object-cover rounded-3xl"
+          />
+        </div>
 
-          <Button disabled={isLoading} type='submit' className='bg-dark-4'>
-
-            {isLoading ? (
-              <>
-                <Loading title='Sign in...' />
-              </>
-            ) : 'Sign in'}
-          </Button>
-        </form>
-        <Link href='/sign-up'>
-          <p className='text-center text-sm mt-4'>
-            Don&apos;t have an account? Sign up
-          </p>
-        </Link>
       </section>
     </main>
   )
--- components/ui/password-input.tsx
+++ components/ui/password-input.tsx
@@ -9,7 +9,7 @@ import { cn } from '@/lib/utils'
 
 const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(({ className, name, ...props }, ref) => {
     const [showPassword, setShowPassword] = React.useState(false)
-    const disabled = props.value === '' || props.value === undefined || props.disabled
+
 
     return (
         <div className="relative">
@@ -26,10 +26,10 @@ const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(({ classNam
                 size="sm"
                 className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
                 onClick={() => setShowPassword((prev) => !prev)}
-                disabled={disabled}
+
             >
-                {showPassword && !disabled ? (
-                    <EyeIcon className="h-4 w-4" aria-hidden="true" />
+                {showPassword ? (
+                    <EyeIcon className="h-4 w-4" aria-hidden="true"  />
                 ) : (
                     <EyeOffIcon className="h-4 w-4" aria-hidden="true" />
                 )}
