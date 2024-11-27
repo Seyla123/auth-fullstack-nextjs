ce201ba315a36ebeef666cc2d440ebcc79070c20
Seav <mrrseyla.758@gmail.com>
2024-11-27 09:47:24 +0700

    feat(resend-invite-user) :add api resend invite user

--- app/api/admin/users/invite/[slug]/resend/route.ts
+++ app/api/admin/users/invite/[slug]/resend/route.ts
@@ -1,9 +1,10 @@
 import { db } from "@/lib/initDb";
+import { sendMail } from "@/lib/server/services/EmailService";
 import AppError from "@/lib/server/utils/appError";
-import { invitedUser } from "@/lib/server/utils/authUtils";
+import { createVerificationToken, invitedUser } from "@/lib/server/utils/authUtils";
 import catchAsync from "@/lib/server/utils/catchAsync";
 import { protect, restrict } from "@/middlewares/server/authMiddleware";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const POST = catchAsync(async (req: NextRequest, { params }: { params: { slug: string } }) => {
     protect(req);
@@ -17,16 +18,43 @@ export const POST = catchAsync(async (req: NextRequest, { params }: { params: { 
 
     // get the current user 
     const currentUserId = JSON.parse(req.headers.get('userId')!);
+
     // find the invited user with invited id and current user id
+    //return with invited user data too
     const invitedUser = db.prepare(`
-        SELECT * FROM invites WHERE id = ? AND invitedBy = ? 
+        SELECT invites.*,users.username as invitedByUsername, users.email as invitedByEmail 
+        FROM invites 
+        LEFT JOIN users ON invites.invitedBy = users.id
+        WHERE invites.id = ? AND invites.invitedBy = ?
         `).get(id, currentUserId) as invitedUser;
 
+    //check if this user has already been registered
     if (invitedUser.status == 'accepted') {
         throw new AppError("This invited user has already been registered", 401);
     }
+    //resend invitation to user email
+    const { token, hashedToken } = createVerificationToken();
+    const expiredDate = new Date(Date.now() + 1000 * 60 * 60); // 1 hour from now
+    const formattedDate = expiredDate.toISOString().replace('T', ' ').slice(0, 19);
+
+    const stmt = db.prepare(`
+        UPDATE invites SET 
+            inviteToken = ?, 
+            expiredAt = ? WHERE id = ? `);
+    stmt.run(hashedToken, formattedDate, id);
+
+    const url = `${req.nextUrl.origin || process.env.NEXTAUTH_URL || 'http://localhost:3000'}/register-invited-user?token=${token}`;
+    const dataSend =
+    {
+        "invitedLink": url,
+        "invitedBy": invitedUser.invitedByEmail
+    }
+    //send email verification token
+    await sendMail(invitedUser.email, 4, dataSend)
+
     return NextResponse.json({
         status: "success",
         message: `resent invitation to ${invitedUser.email} successfully`,
+        invitedUser
     }, { status: 200 })
 });
